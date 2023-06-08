d257a69b02ebf7c2d35118416367c04c5e76dfce
bokutotu <mushin.hudoushin@gmail.com>
2023-06-08 23:40:10 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by HikaruKondo on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
